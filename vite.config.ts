9d35dcc9530459bb365a6a400dcda11412a42c2f
MeowningMaster <meowningmaster@gmail.com>
2023-07-16 14:53:56 +0300

    set base everywhere

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solid from 'vite-plugin-solid'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+    base: '/frontend/',
     plugins: [
         solid(),
         // VitePWA({
