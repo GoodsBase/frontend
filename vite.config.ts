63614a93cbc9847ddd9b1e7a57463771a4fbee98
MeowningMaster <meowningmaster@gmail.com>
2023-07-18 12:59:33 +0300

    pwa mvp

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,14 @@ export default defineConfig({
             manifest: {
                 name: 'GoodsBase',
                 short_name: 'GoodsBase',
+                description: 'Веб-застосунок для обліку товарів',
+                theme_color: '#ffffff',
                 icons: [
                     {
                         src: 'favicon.svg',
                         type: 'image/svg+xml',
-                        purpose: 'any',
+                        sizes: '72x72 96x96 120x120 128x128 144x144 152x152 180x180 192x192 384x384 512x512',
+                        purpose: 'any maskable',
                     },
                 ],
                 start_url: '/',
